2e289f0bf7bd1d7d7a91b9b787eb84687a3a439e
Suwanda Kurnia Maufdi <63093353+suwandakurnia@users.noreply.github.com>
2022-04-22 22:02:41 +0700

    Add files via upload

---
+++ Python/Kelas Persiapan/Introduction to Data Science with Python.py
@@ -0,0 +1,19 @@
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+#Tugas Praktek
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+#Menggunakan Library di Python
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
